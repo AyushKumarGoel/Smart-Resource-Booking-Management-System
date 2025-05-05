b1cfa3fb3a8c78feea63149cc6b494931ba178b3
AYUSH KUMAR GOEL <ayushkumargoel804@gmail.com>
2025-05-05 13:43:34 +0530

    Delete SRBMS_Project/srmbs directory

--- SRBMS_Project/srmbs/utils/ConsoleUI.java
+++
@@ -1,9 +0,0 @@
-package srmbs.utils;
-
-public class ConsoleUI {
-    public static void showWelcome() {
-        System.out.println("=================================");
-        System.out.println(" Welcome to SRBMS System ");
-        System.out.println("=================================");
-    }
-}
